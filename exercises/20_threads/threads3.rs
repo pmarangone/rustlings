a5ee059b745b33e6d35f47b5662037b3b8f5d4c0
pmarangone <patrickmarangone@gmail.com>
2025-02-09 13:22:43 -0300

    wip

--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -36,7 +36,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
             tx1.send(*val).unwrap();
-            thread::sleep(Duration::from_secs(1));
+            // thread::sleep(Duration::from_secs(1));
         }
     });
 
@@ -44,12 +44,12 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
             tx2.send(*val).unwrap();
-            thread::sleep(Duration::from_secs(1));
+            // thread::sleep(Duration::from_secs(1));
         }
     });
 }
 
-#[test]
+// #[test]
 fn main() {
     let (tx, rx) = mpsc::channel();
     let queue = Queue::new();
